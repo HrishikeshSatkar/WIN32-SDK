c3e992b94cfc2764b5015426f4e270c25a957133
HrishikeshSatkar <hrishikeshsatkar007@gmail.com>
2025-09-07 06:38:43 +0530

    UWP

---
+++ 05-UWP/01-HelloWorld/Hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace Platform;
+using namespace std;
+
+int main(Array <String^>^ args)
+{
+	//code
+	String^ str = "Hello World!!!";
+	wcout << str->Data() << endl;
+	return 0;
+}
+
+
+
